77b1d716ccfa89861b16fbf572333169cf876636
Goetz van Rissenbeck <goetz@vanrissenbeck.de>
2017-08-25 18:44:41 +0200

    added linklog partials, still some layout to be done

--- static/config.rb
+++ static/config.rb
@@ -20,5 +20,5 @@ line_comments = false
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
-# and then run:
+# and then run:sass-convert -R --from scs
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
